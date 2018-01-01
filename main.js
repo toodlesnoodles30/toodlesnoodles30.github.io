30c769094682b8a551839d36804393aa92f0a461
Ming <toodlesnoodles30@gmail.com>
2018-01-01 15:40:14 -0800

    Maps changes

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 // initialize the configuration of map
 function initMap() {
-    
-var marker = new google.maps.Marker({ //use Marker constructor to add a marker to map
-      position: {lat: 40.8054491, lng: -73.9654415},
-      map: map,
-    });
-   
-};
+
+
 
 
 initMap();
