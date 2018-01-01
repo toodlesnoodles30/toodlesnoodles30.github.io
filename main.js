199fa124383786133d581a8b41d2aa9b64347fab
Ming <ming@manifold.co>
2018-01-01 15:10:36 -0800

    Tom's restuarant html

---
+++ main.js
@@ -0,0 +1,12 @@
+// initialize the configuration of map
+function initMap() {
+    
+var marker = new google.maps.Marker({ //use Marker constructor to add a marker to map
+      position: {lat: 40.8054491, lng: -73.9654415},
+      map: map,
+    });
+   
+};
+
+
+initMap();
